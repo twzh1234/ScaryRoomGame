53b7768595a011e2c257279d83891b5e3ab088bc
Jia Zhou <jiaz2@illinois.edu>
2018-04-16 20:36:03 -0500

    Level 3 (1st version)

--- Level3.py
+++ Level3.py
@@ -44,6 +44,18 @@ class Player(pygame.sprite.Sprite):
             self.speedy = 5
         self.rect.x += self.speedx
         self.rect.y += self.speedy
+        if self.rect.right > width:
+            self.rect.right = width
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.bottom > height:
+            self.rect.bottom = height
+        if self.rect.top < 0:
+            self.rect.top = 0
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx,self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
 class Enemy(pygame.sprite.Sprite):
     # sprite for player
@@ -61,9 +73,49 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.bottom > height - 200:
             self.y_speed = -6
         if self.rect.top < 200:
-            self.y_speed = 8
+            self.y_speed = 6
         if self.rect.left > width:
             self.rect.right = 0
+
+
+
+class Ghost(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(os.path.join(img_folder,"Ghost2.png")).convert()
+        self.image.set_colorkey(black)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width - self.rect.width)
+        self.rect.y = random.randrange(-100,-40)
+        self.speedy = random.randrange(3,8)
+        self.speedx = random.randrange(-3,3)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.top > height + 10 or self.rect.left < -25 or self.rect.right > width + 20:
+            self.rect.x = random.randrange(width - self.rect.width)
+            self.rect.y = random.randrange(-100,40)
+            self.speedy = random.randrange(3,8)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10,10))
+        self.image.fill((255,255,0))
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.bottom < 0:
+            self.kill()
+
+
+
 
 # initialize window
 pygame.init()
@@ -73,10 +125,18 @@ pygame.display.set_caption("level 3")
 clock = pygame.time.Clock()
 
 all_sprites = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
 player = Player()
 enemy = Enemy()
 all_sprites.add(player)
 all_sprites.add(enemy)
+
+Ghosts = pygame.sprite.Group()
+Ghost.add(enemy)
+for i in range(8):
+    m = Ghost()
+    Ghosts.add(m)
+    all_sprites.add(m)
 # game loop
 
 running = True
@@ -86,8 +146,24 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
+            if event.key == pygame.K_ESCAPE:
+                running = False
 
     all_sprites.update()
+
+    # check collision with ghost
+    hits = pygame.sprite.groupcollide(Ghosts,bullets,True,True)
+    for hit in hits:
+        m = Ghost()
+        all_sprites.add(m)
+        Ghosts.add(m)
+    #check collision
+    #hits1 = pygame.sprite.spritecollide(player,Ghosts,False)
+    #if hits1:
+        #running = False
 
     screen.fill(darkred)
     all_sprites.draw(screen)
